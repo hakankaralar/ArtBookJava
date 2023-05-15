e83e2113c30497aed26e3581eee000f6244afcb5
hakankaralar <hakankaralar27@gmail.com>
2023-05-16 00:08:59 +0300

    ArtBookJava

---
+++ ArtBookJava/app/src/main/java/com/hakankaralar/artbookjava/Art.java
@@ -0,0 +1,13 @@
+package com.hakankaralar.artbookjava;
+
+public class Art {
+
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+}
